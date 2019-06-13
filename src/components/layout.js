360e13a0329f81b37461b3e889232676be2af5c8
Shogo Ishimaru <hakanai.blog@gmail.com>
2019-06-13 20:48:40 +0900

    remove default styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,14 +26,7 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div className="container">
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
