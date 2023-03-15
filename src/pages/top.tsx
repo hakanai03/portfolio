c9bf85e682a7dbd9a850f71639f61ba38a497e32
hakanai03 <hakanai.blog@gmail.com>
2023-03-15 22:45:20 +0900

    wip

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -10,46 +10,57 @@ export const Top = () => {
     <Background
       style={{
         height: "100svh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "2rem",
+        overflow: "hidden",
       }}
     >
       <div
         style={{
-          borderRadius: "50%",
-          width: "5rem",
-          height: "5rem",
-          overflow: "hidden",
-        }}
-      >
-        <img
-          style={{ width: "5rem", height: "100%" }}
-          src={gravatarURL}
-          alt="Gravatar Icon"
-        />
-      </div>
-      <p>I'm shogo.</p>
-      <p>
-        Software Developer (Full Stack) / Web UI Designer / Infrastructure
-        (Cloud, On-premise)
-      </p>
-      <div
-        style={{
+          margin: "1rem",
+          height: "100%",
           display: "flex",
-          gap: "1rem",
+          flexDirection: "column",
           justifyContent: "center",
+          gap: "2rem",
           alignItems: "center",
         }}
       >
-        <a href="https://github.com/hakanai03">
-          <GithubOutlined style={{ fontSize: "2rem" }} />
-        </a>
-        <a href="https://twitter.com/i_shogo03" style={{ fontSize: "2rem" }}>
-          <TwitterOutlined />
-        </a>
+        <div
+          style={{
+            borderRadius: "50%",
+            width: "5rem",
+            height: "5rem",
+            overflow: "hidden",
+          }}
+        >
+          <img
+            style={{ width: "5rem", height: "100%" }}
+            src={gravatarURL}
+            alt="Gravatar Icon"
+          />
+        </div>
+        <p>I'm shogo.</p>
+        <div style={{display: "flex", justifyContent: "center"}}>
+          <p>Software Developer (Full Stack)</p>
+          <p> / </p>
+          <p>Web UI Designer</p>
+          <p> / </p>
+          <p>Infrastructure (Cloud, On-premise)</p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            gap: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <a href="https://github.com/hakanai03">
+            <GithubOutlined style={{ fontSize: "2rem" }} />
+          </a>
+          <a href="https://twitter.com/i_shogo03" style={{ fontSize: "2rem" }}>
+            <TwitterOutlined />
+          </a>
+        </div>
       </div>
     </Background>
   );
