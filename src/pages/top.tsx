f162865f915806b5009e921056800bac043bd8e0
hakanai03 <hakanai.blog@gmail.com>
2023-03-15 23:23:19 +0900

    update

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,4 +1,5 @@
 import { GithubOutlined, TwitterOutlined } from "@ant-design/icons";
+import { Col, Grid, Row } from "antd";
 import md5 from "md5";
 import { Background } from "../features/animations/Background";
 
@@ -6,6 +7,9 @@ const hash = md5("hakanai.blog@gmail.com");
 const gravatarURL = `https://www.gravatar.com/avatar/${hash}?s=80`;
 
 export const Top = () => {
+  const { md } = Grid.useBreakpoint();
+  const span = md ? undefined : 24;
+
   return (
     <Background
       style={{
@@ -20,36 +24,55 @@ export const Top = () => {
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-          gap: "2rem",
+          gap: "3rem",
           alignItems: "center",
         }}
       >
         <div
           style={{
-            borderRadius: "50%",
-            width: "5rem",
-            height: "5rem",
-            overflow: "hidden",
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+            alignItems: "center",
           }}
         >
-          <img
-            style={{ width: "5rem", height: "100%" }}
-            src={gravatarURL}
-            alt="Gravatar Icon"
-          />
+          <div
+            style={{
+              borderRadius: "50%",
+              width: "5rem",
+              height: "5rem",
+              overflow: "hidden",
+            }}
+          >
+            <img
+              style={{ width: "5rem", height: "100%" }}
+              src={gravatarURL}
+              alt="Gravatar Icon"
+            />
+          </div>
+          <p>I'm shogo.</p>
         </div>
-        <p>I'm shogo.</p>
-        <div style={{display: "flex", justifyContent: "center"}}>
-          <p>Software Developer (Full Stack)</p>
-          <p> / </p>
-          <p>Web UI Designer</p>
-          <p> / </p>
-          <p>Infrastructure (Cloud, On-premise)</p>
-        </div>
+        <Row justify="center" gutter={[16, 16]} style={{ textAlign: "center" }}>
+          <Col xs={span} sm={span}>
+            Software Developer (Full Stack)
+          </Col>
+          <Col xs={span} sm={span}>
+            /
+          </Col>
+          <Col xs={span} sm={span}>
+            Web UI Designer
+          </Col>
+          <Col xs={span} sm={span}>
+            /
+          </Col>
+          <Col xs={span} sm={span}>
+            Infrastructure (Cloud, On-premise)
+          </Col>
+        </Row>
         <div
           style={{
             display: "flex",
-            gap: "1rem",
+            gap: "1.5rem",
             justifyContent: "center",
             alignItems: "center",
           }}
