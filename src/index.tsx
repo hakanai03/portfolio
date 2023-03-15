58c6b04dd478f86fa8f84564efcc132be5eb70fd
hakanai03 <hakanai.blog@gmail.com>
2023-03-15 23:35:45 +0900

    bugfix

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import "./index.css"
+import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Top } from "./pages/top";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Top />
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: '"Orbitron", sans-serif;',
+        },
+      }}
+    >
+      <Top />
+    </ConfigProvider>
   </React.StrictMode>
 );
-
